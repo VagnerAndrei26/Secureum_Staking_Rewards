8cab22b91c6c634bd0899f84fccacd2a51604491
VagnerAndrei26 <vagner@DESKTOP-V309Q2O>
2023-03-16 13:04:52 +0200

    Final commit

---
+++ .certora_internal/23_03_15_22_29_26_805/.certora_config/autoFinder_DummyERC20A.sol_0/0_autoFinder_DummyERC20A.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity >= 0.8.0;
+import '/home/vagner/Secureum_Staking_Rewards/.certora_internal/23_03_15_22_29_26_805/.certora_sources/autoFinder_DummyERC20Impl.sol';
+
+contract DummyERC20A is DummyERC20Impl {}
